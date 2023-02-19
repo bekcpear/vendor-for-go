615eac8a3ebb7120f3a780cb2460a9555b5658f1
Ryan Qian <i@bitbili.net>
2023-02-19 15:50:14 +0800

    .do/v2ray-core/update-quic-go.sh: update
    1. no longer use unsafe-assume-no-moving-gc
    2. update to respect the renamed quic-go path

--- .do/v2ray-core/update-quic-go.sh
+++ .do/v2ray-core/update-quic-go.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #
 
-# quic-go and assume-no-moving-gc are tierd to the version
-# of the go compiler, but over time, there is no guarantee
+# quic-go is tierd to the version of the go compiler and is
+# directly depended by github.com/v2fly/v2ray-core/v5/app/dns,
+# but over time, there is no guarantee
 # that quic-go will be updated instantly every time, so it
 # should be checked when package v2ray-core and use the
 # updated go.mod and go.sum
 pushd "${MOD_DIR}"
-go get -u go4.org/unsafe/assume-no-moving-gc
-go get -u github.com/lucas-clemente/quic-go
+go get github.com/quic-go/quic-go@latest
