8f9601861efedf3f0a7fd0a35979ac0a0b477372
Ryan Qian <i@bitbili.net>
2023-10-22 23:21:23 +0800

    headscale: fix gen scripts

--- .do/headscale/generate-proto.sh
+++ .do/headscale/generate-proto.sh
@@ -7,17 +7,21 @@ rm -rf ./gen
 _BUF_CMD="${GOPATH}/bin/buf"
 if [[ ! -x ${_BUF_CMD} ]]; then
   go install github.com/bufbuild/buf/cmd/buf@latest
-  deps=(google.golang.org/protobuf/cmd/protoc-gen-go
-        google.golang.org/grpc/cmd/protoc-gen-go-grpc
-        github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2
-        github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway)
-  for dep in ${deps[@]}; do
-    go install ${dep} || \
-      go install ${dep}@latest
-  done
+#  deps=(google.golang.org/protobuf/cmd/protoc-gen-go
+#        google.golang.org/grpc/cmd/protoc-gen-go-grpc
+#        github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2
+#        github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway)
+#  for dep in ${deps[@]}; do
+#    go install ${dep} || \
+#      go install ${dep}@latest
+#  done
 fi
 ${_BUF_CMD} generate proto
+
+if [[ -d "${VCS_DIR}" ]]; then
+  rm -rf "${VCS_DIR%/}/gen"
+fi
 cp -a ./gen ${VCS_DIR%/}${VCS_DIR:+/}
 git -C ${VCS_DIR} add ./gen
-git -C ${VCS_DIR} commit -m 'gen: add generated proto'
+git -C ${VCS_DIR} commit -m "gen: update generated proto for ${VERSION}"
 popd
